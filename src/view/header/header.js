e735325be9841ced030858abd067bd8868e49f45
LucioMB <57898810+LucioMB@users.noreply.github.com>
2020-01-15 18:57:53 +0100

    Add files via upload

--- src/view/header/header.js
+++ src/view/header/header.js
@@ -1,18 +1,14 @@
 import React from 'react';
-//import styled from 'styled-components';
 import styled, { css } from 'styled-components';
-//import '../../css/header-sc.css';
 import '../../css/header.css';
-//import renderer from 'react-test-renderer';
-//import 'jest-styled-components';
 
 
 const Container = styled.header`
     position: fixed;
     width: 100%;
-    height: 70px;
+    height: auto;
     margin: 0 0 0 0;
-    padding: 10px 10px 0 15px;
+    padding: 0 0 0 0;
     background-color: transparent;
     grid-column: auto;
     border: 0px solid #000;
@@ -21,54 +17,78 @@ const Container = styled.header`
 `;
 
 const Nav = styled.div`
-    height: 50px;
     margin: 0 0 0 0;
-    padding: 0 10px 0 15px;
+    padding: 0 0 0 0;
     border: 0px solid #000;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
 
     ${props => props.primary && css`
     padding: 0 50px;
   `}
 `;
 
-const Anchor = styled.a`
+const Anchor = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
     text-decoration: none;
-    font-size: 10px;
-    font-weight: lighter;
-    color: #84eced;
     margin-right: 20px;
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    border: 0px solid #000;
+    height: 70px;
+
 
     ${props => props.primary && css`
+    align-content: flex-end;
+    `}
+`;
+
+const Sp = styled.a`
     font-style: normal;
     font-size: 20px;
     font-weight: lighter;
     margin: 60px;
     color: #84eced;
-    `}
-`
+`;
+
+const Wis = styled.a`
+    font-size: 10px;
+    font-weight: lighter;
+    color: #84eced;
+    margin-right: 20px;
+`;
+
+const Hdw = styled.a`
+    font-size: 10px;
+    font-weight: lighter;
+    color: #84eced;
+    margin-right: 20px;
+`;
+
+const Ju = styled.a`
+    font-size: 10px;
+    font-weight: lighter;
+    color: #84eced;
+    margin-right: 20px;
+    align-content: flex-end;
+`;
+
 
 function Header() {
         return(
         <>
             <div  id="top"></div>
             <Container>
-                <header>
                     <Nav>
                         <Anchor>
-                            <a primary href="#top">SPLUGA</a>
+                            <Sp primary href="#top">SPLUGA</Sp>
                                 <div primary>
-                                    <a href="#what-is-spluga">WHAT IS SPLUGA?</a>
-                                    <a href="#how-does-it-work">HOW DOES IT WORK?</a>
-                                    <a href="#join-us">JOIN US!</a>
+                                    <Wis href="#what-is-spluga">WHAT IS SPLUGA?</Wis>
+                                    <Hdw href="#how-does-it-work">HOW DOES IT WORK?</Hdw>
+                                    <Ju href="#join-us">JOIN US!</Ju>
                                 </div>
                         </Anchor>
                     </Nav>
-                </header>
             </Container>
         </>
     )
