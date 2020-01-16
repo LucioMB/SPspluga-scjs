523ec3d4d60d23c552e181c632c4abbd48f31d7f
LucioMB <57898810+LucioMB@users.noreply.github.com>
2020-01-16 19:43:30 +0100

    Add files via upload

--- src/view/footer/footer.js
+++ src/view/footer/footer.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import '../../css/footer.css';
+import styled from 'styled-components';
 
+
+
+const Container = styled.div`
+    width: 100%;
+    height: auto;
+    align-items: center;
+    background-color: #222;
+    margin: -30px 0 0 0;
+
+    & h6 {
+    color: #999;
+    text-align: center;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    font-weight: lighter;
+    padding: 30px 0 0 0;
+    }
+`;
 
 function Footer() {
     return(
-            <footer>
-                <h6 class="text-footer">
+            <Container>
+                <h6>
                     Copyright © 2018 mOndOra. All Rights Reserved.
                     <br />
                     mOndOra s.r.l. S.B. - Via Uberto Visconti di Modrone 33 - 20122 Milano c.f. e p. iva 03680680968 cap. soc. 105.000 i.v. - Rea n. 1694989
@@ -16,8 +33,8 @@ function Footer() {
                     <br />
                     Privacy 
                 </h6>
-            </footer>
+            </Container>
     )
 }
-
+ 
 export default Footer;
