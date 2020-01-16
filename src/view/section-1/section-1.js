633d620f82e4afcc837b3353939c5dd5041326cd
LucioMB <57898810+LucioMB@users.noreply.github.com>
2020-01-16 19:44:33 +0100

    Add files via upload

--- src/view/section-1/section-1.js
+++ src/view/section-1/section-1.js
@@ -13,6 +13,7 @@ const Container = styled.section`
     flex-direction: row;
     align-items: center;
     padding-top: 200px;
+    padding-bottom: 300px;
     height: 100%;
     width: 100%;
 `;
@@ -24,21 +25,19 @@ const Text = styled.div`
     font-style: normal;
     color: #fff;
     padding: 0 0 0 80px;
-    margin: 0 0 50px 100px;
+    margin: 0 0 0 100px;
 
     ${
         props => props.primary && css`
-        margin: 80px;
-    }
-`
-    }
+        margin: 90px 0 90px 0;
+`}
 
 `;
 
 const Logo = styled.img`
-    width: 480px;
+    width: 560px;
     height: auto;
-    padding: 0 50px 10px 70px;
+    padding: 100px 50px 10px 70px;
     border: 0px solid #000;
     vertical-align: top;
     margin-right: 150px;
@@ -54,7 +53,7 @@ const Anchor = styled.a`
     padding: 20px 60px 20px 60px;
     border: 2px solid #fff;
     border-radius: 50px;
-    margin: 50px 0 0 0;
+    margin: 0px 0 0 0;
     font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
     font-size: 12px;
     font-weight: normal;
